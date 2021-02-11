41a8ba0e01f62665e76e4fe45a9dfb16b4a1e6c9
Wilson Pinto <wilson27pinto@gmail.com>
2021-02-11 22:40:58 +0530

    added header, button and tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Tasks = () => {
+
+    const tasks = [
+        {
+            "id": 1,
+            "text": "Doctors Appointment",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+        },
+        {
+            "id": 2,
+            "text": "Meeting at School",
+            "day": "Feb 6th at 1:30pm",
+            "reminder": true
+        }
+    ]
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
+        </>
+    )
+}
+
+export default Tasks
